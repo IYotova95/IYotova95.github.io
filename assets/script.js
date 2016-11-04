89eb0c4e37c5c9b26b1f7a09fffadcbc0cdf103e
IYotova95 <student@bcdm52562.local>
2016-11-04 11:07:07 -0400

    fixed code

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,11 @@ $( document ).ready(function() {
 
 });
 $(document).ready(function)) {
-	$('h3').hover(function) {
-		$('h3').css('background-color','yellow')};
+
+	$('h3').hover(
+		function) {
+		$('h3').css('background-color','yellow');
 	}
-};
+);
 });
+
