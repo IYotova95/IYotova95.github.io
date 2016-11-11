f8df3f9f3087bce3d6aaa5c0e0f7f0097597fdbc
IYotova95 <student@bcdm52562.local>
2016-11-11 11:51:04 -0500

    typpooooo

--- assets/script.js
+++ assets/script.js
@@ -8,37 +8,37 @@ $( document ).ready(function() {
 // 
 // when user clicks on page
 // get horizontal position from event
-		var yPosition,xPosition,screenHeight,screenWidth,cssValue,lightnessValue,saturationValue,saturationRatio,lightnessRatio,hueValue,cssValue;
+	var yPosition,xPosition,screenHeight,screenWidth,cssValue,lightnessValue,saturationValue,saturationRatio,lightnessRatio,hueValue,cssValue;
 
-		$('body'),click(
-			function(event){
-				xPosition = event.pageX;
-				// get width of screen
-				screenWidth = $('body').width();
-				// divide position by width to get proportion
-				saturationRatio = xPosition/screenWidth;
-				// saturationvalue = proportion * 100
-				saturationValue = saturationRatio * 100;
-				// get vertical position from event
-				yPosition = event.pageY;
-				// get height of screen
-				screenHeight = $('body').height();
-				// divide position by height to get proportion
-				lightnessRatio = yPosition/screenHeight;
-				// lightnessValue = proportion * 100
-				lightnessValue = lightnessRatio * 100;
+	$('body').click(
+		function(event){
+			xPosition = event.pageX;
+			// get width of screen
+			screenWidth = $('body').width();
+			// divide position by width to get proportion
+			saturationRatio = xPosition/screenWidth;
+			// saturationvalue = proportion * 100
+			saturationValue = saturationRatio * 100;
+			// get vertical position from event
+			yPosition = event.pageY;
+			// get height of screen
+			screenHeight = $('body').height();
+			// divide position by height to get proportion
+			lightnessRatio = yPosition/screenHeight;
+			// lightnessValue = proportion * 100
+			lightnessValue = lightnessRatio * 100;
 
 
-			}
-		);
+		}
+	);
 
-		hueValue = Math.random() * 360;
-		// plug in three values to css using string concatenation: +
-		// embed saturation value in css rule
-		cssValue = 'hsl('+hueValue+', '+saturationValue+'%, '+ (lightnessValue)%100 +'%)';
-		// change css rule for body with jquery	
-		// change the css value to color w new saturation.
-		$('body').css('background-color', cssValue);
+	hueValue = Math.random() * 360;
+	// plug in three values to css using string concatenation: +
+	// embed saturation value in css rule
+	cssValue = 'hsl('+hueValue+', '+saturationValue+'%, '+ (lightnessValue)%100 +'%)';
+	// change css rule for body with jquery	
+	// change the css value to color w new saturation.
+	$('body').css('background-color', cssValue);
 
 
 // 
